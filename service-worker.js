e9b51c32cce2393f2dbbe146c5e93f7765191c6b
DFL-Reiner <reiner@defiland.app>
2021-07-14 22:08:00 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8e85aebaa06a73552e730a89944fbf00.js"
+  "./precache-manifest.c5827af6bd856b422cbd4560b6ac8361.js"
 );
 
 self.addEventListener('message', (event) => {
